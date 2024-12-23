46e705b4de4177cf5fa1be4b2a248c2c0ae36e00
wyattjabroni <wyattj28@gmail.com>
2024-12-23 15:15:46 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -48,7 +48,7 @@ export const managers = [
     {
      // "roster": 2,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "neresonbe",
-      "managerID" : "1004151084518084608"
+      "managerID" : "1004151084518084608",
       //"tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       //"location": "Brooklyn", // (optional)
       "bio": "No Rebuild Needed",
